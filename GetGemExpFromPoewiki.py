4752f770d45ef7301f0b0e06062f76acad8a8346
nick-canale <114369302+nick-canale@users.noreply.github.com>
2022-10-08 13:14:31 -0400

    Pull gem exp from poe wiki

---
+++ GetGemExpFromPoewiki.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import sqlite3
+
+query = 'cargoquery'
+url = 'https://www.poewiki.net/api.php'
+params = {
+    'action' : 'cargoquery',
+    'format' : 'json',
+    'tables' : 'skill_levels',
+    'fields' : '_pageName=PageName, level, experience ',
+    'where' : 'experience <> 0',
+    # 'group by' : '_pageName',
+    'limit' : 500
+}
+data = requests.get(url, params=params).json()
+
+GemExp = []
+
+for i in data['cargoquery']:
+    GemExp.append([i['title']['PageName'], i['title']['level'],i['title']['experience']])
+
+# save GemExp to sqlite table
+con = sqlite3.connect(f'Gems.db')
+
+cur = con.cursor()
+
+try:
+    res = cur.execute(f'select name from sqlite_master where name = \'GemExp\'')
+    if res.fetchone() is not None:
+        cur.execute(f'drop table GemExp')    
+except sqlite3.OperationalError:
+    print(f'GemExp table was not created previously.')
+        
+cur.execute(f'create table GemExp(GemID INTEGER PRIMARY KEY AUTOINCREMENT, GemName, level, experience)')
+
+
+cur.executemany('insert into GemExp (GemName, level, experience) values(?,?,?)', GemExp)
+
+con.commit()
+

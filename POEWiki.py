aaf227535a1b4cd670ce1d7fa01b3f1a53b7ec93
Nick Canale <nick.canale@gmail.com>
2022-10-24 17:03:34 -0400

    Fixed query to now get only max level gems

--- POEWiki.py
+++ POEWiki.py
@@ -3,20 +3,36 @@ import json
 import sqlite3
 
 def GetGemEXPFromPOEWiki():
+    GemExp = []
     query = 'cargoquery'
     url = 'https://www.poewiki.net/api.php'
+
+    #This one gets all normal gems
     params = {
         'action' : 'cargoquery',
         'format' : 'json',
         'tables' : 'skill_levels',
         'fields' : '_pageName=PageName, level, experience ',
-        'where' : 'experience <> 0',
+        'where' : 'experience <> 0 and level = 20',
         # 'group by' : '_pageName',
         'limit' : 500
     }
     data = requests.get(url, params=params).json()
 
-    GemExp = []
+    for i in data['cargoquery']:
+        GemExp.append([i['title']['PageName'], i['title']['level'],i['title']['experience']])
+
+    #this one gets all non-normal gems (awakened, enlighten, etc.)
+    params = {
+        'action' : 'cargoquery',
+        'format' : 'json',
+        'tables' : 'skill_levels',
+        'fields' : '_pageName=PageName, level, experience ',
+        'where' : 'experience > 308712291 and level < 7',
+        # 'group by' : '_pageName',
+        'limit' : 500
+    }
+    data = requests.get(url, params=params).json()
 
     for i in data['cargoquery']:
         GemExp.append([i['title']['PageName'], i['title']['level'],i['title']['experience']])
